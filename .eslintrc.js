cfe36e309d06973708939ba591ab7b971e9853e6
Gottfried Kleinberger <friedi.frieds@gmail.com>
2021-09-29 19:20:02 +0000

    environment set-up

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+// module.exports = {
+//     "env": {
+//         "browser": true,
+//         "es2021": true
+//     },
+//     "extends": "eslint:recommended",
+//     "parserOptions": {
+//         "ecmaVersion": 12,
+//         "sourceType": "module"
+//     },
+//     "rules": {
+//         "indent": [
+//             "error",
+//             4
+//         ],
+//         "linebreak-style": [
+//             "error",
+//             "unix"
+//         ],
+//         "quotes": [
+//             "error",
+//             "double"
+//         ],
+//         "semi": [
+//             "error",
+//             "always"
+//         ]
+//     }
+// };
+
+module.exports = {
+  'extends': 'lighthouselabs',
+};
